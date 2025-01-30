e7d3561e9373c34de6756dc85309b20a3bfa028e
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-01-30 17:28:58 +0000

    added accountState

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,12 @@
 const accountId = 144553
 let accountEmail = "saurabh@google.com"
-var accountPassword = "12345"
-accountCity = "Greater Noida"
+var accountPassword = "12345" // don't use 
+accountCity = "Greater Noida" //don't use
+let accountState;
 
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
 
 // accountId = 2 // not allowed
 
@@ -12,3 +16,4 @@ accountCity = "Azamgarh"
 
 console.log(accountId);
 console.table([accountEmail, accountId, accountPassword, accountCity]);
+console.log(accountState); //undefined
