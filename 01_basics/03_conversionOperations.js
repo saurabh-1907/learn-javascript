c962762dd913b15c21f5b061c159ef9c82f53c1b
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-01-30 20:17:18 +0000

    conversion of dt

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,68 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let scor = null
+
+console.log(typeof scor);
+console.log(typeof(scor));
+
+let valueInNumbe = Number(scor)
+console.log(typeof valueInNumbe);
+console.log(valueInNumbe);
+
+let scor1 = undefined
+
+console.log(typeof scor1);
+console.log(typeof(scor1));
+
+let valueInNumbe1 = Number(scor1)
+console.log(typeof valueInNumbe1);
+console.log(valueInNumbe1);
+
+let scor2 = true
+
+console.log(typeof scor2);
+console.log(typeof(scor2));
+
+let valueInNumbe2 = Number(scor2)
+console.log(typeof valueInNumbe2);
+console.log(valueInNumbe2);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let isLoggedIn1 = 1
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1);
+
+let isLoggedIn2 = 3
+
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2);
+
+let isLoggedIn3 = "Saurabh"
+
+let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
+console.log(booleanIsLoggedIn3);
+
+let isLoggedIn4 = ""
+
+let booleanIsLoggedIn4 = Boolean(isLoggedIn4)
+console.log(booleanIsLoggedIn4);
+
+// 0 => f
