2eba574f6660fdba5002108664524ae85074b12f
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-01-30 17:56:49 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert("hello") we are using node js not browser
+// code readability should be high
+let name = "saurabh"
+let age = 22
+let isLoggedIn = false
+
+// number 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null is a standalone value empty value let state = null temp zero nahi hoga null hoga if not available
+// undefined => let state abhi value assign nahi hui hai
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // null is a object
+console.log(typeof undefined);
+
+
+
