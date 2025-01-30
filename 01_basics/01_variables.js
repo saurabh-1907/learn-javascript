93d3a4498ed2b239d210acb2cb82e12aa685189a
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-01-30 17:25:12 +0000

    lesson 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "saurabh@google.com"
+var accountPassword = "12345"
+accountCity = "Greater Noida"
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "sp@sp.com"
+accountPassword = "215535566"
+accountCity = "Azamgarh"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity]);
