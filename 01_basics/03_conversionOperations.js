1b9bf8387e762a24a0f63ccf8b4d225f6ec56ca2
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-02-02 11:25:07 +0000

    3 4 5 complete

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -66,3 +66,32 @@ let booleanIsLoggedIn4 = Boolean(isLoggedIn4)
 console.log(booleanIsLoggedIn4);
 
 // 0 => f
+
+// ******** OPERATIONS ************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " saurabh"
+let str3 = str1 + str2
+ console.log(str3);
+ 
+
+ console.log(typeof("1" + 2)+"1" + 2);
+ console.log(typeof(1 + "2"),1 + "2");
+ console.log(typeof(1+2+"3")+(1+2+"3"));
+ 
+console.log(+true);
+
+console.log(true);
+console.log(+"");
+ 
+ let num1, num2, num3
+ num1 = num3 = num2 = 2 + 2
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter++ + " "+ ++gameCounter);
+ 
---
+++ 01_basics/05_dataTypesSummer.js
@@ -0,0 +1,18 @@
+// Primitive 
+// 7  types : String Number Bollean null undefined  Sysmbol BigInt
+
+// Non Primitive Reference
+// Arrays, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null // Empty
+
+let userEmail;
+let userEmail1 = undefined
+ // Both ways to give value undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+ console.log(id === anotherId);
+ 
