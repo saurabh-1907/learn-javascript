bda01d0c672ff08966411a86ad806f8928fb8200
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-02-08 15:18:24 +0000

    control flow if else

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,21 @@
+// if(condition){
+    
+// }
+const isUserLoggedIn = true
+if(isUserLoggedIn){
+
+}
+
+//==(Check) , != , === type bhi check karta hai
+
+if(2 == "2"){
+    console.log("==");
+}
+
+if(false) console.log("test1"),
+console.log("test2");
+// unreadable but works
+const debitCard =true
+if(isUserLoggedIn && debitCard){
+    console.log("true");
+}
---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,23 @@
+// switch(key){
+//     case value:
+//         break;
+//     default :
+//     break;
+// }
+
+const month = 3
+switch(month){
+    case 1:
+        console.log("Janauary");
+    break; 
+    case 2:
+        console.log("February");// jo code execute hua hai uske niche ka sara ho jata hai issiliye default use karte hai
+// except default
+    break; 
+    case 3:
+        console.log("March");
+    break; 
+    default:
+        console.log("God bless you my son");
+    break;        
+}
