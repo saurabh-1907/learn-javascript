02d9fdd0629f221a5ae59c169b7d09e9e9821640
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-02-08 12:16:23 +0000

    Objects 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,7 +21,54 @@ console.log(regularuser.fullname?.userfullname.lastname);
 
 const obj1 = {1  : "A", 2 : "B"}
 const obj2 = {2  : "A", 4 : "B"}
+const obj4 = {6  : "A", 7 : "B"}
+// const obj3 = {
+//     obj1, obj2
+// }
+// const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = {
-    
-}
+const obj3 = {...obj1,...obj4}
+console.log(obj3);
+
+const users = [
+    user1 = {
+        id : 1,
+        email : "pehla@gmail.com",
+
+
+    },
+    user1 = {
+        id : 1,
+        email : "pehla@gmail.com",
+        
+
+    }
+    ,
+    user3 = {
+        id : 3,
+        email : "teesra@gmail.com",
+        
+
+    }
+    ,
+    user2 = {
+        id : 2,
+        email : "dusra@gmail.com",
+        
+
+    }
+]
+
+users[1].emaillog
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
