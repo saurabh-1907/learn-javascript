7ba18318f9987fdcdc70cd97ef5c873fb282d826
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-04-01 13:37:18 +0000

    templet literal

---
+++ 12_templetLiteral/01_templetLiteral.js
@@ -0,0 +1,16 @@
+const person = {
+    name: "Saurabh Pandey",
+    company: "NIET",
+    course: "Python",
+    website: "www.daddy.com"
+}
+
+var r = 10;
+const pi = 3.145;
+let mesg = "My name is";
+
+let tmsg = `My name is ${person.name}. I study at ${person.company}. The area of circle is ${r*r*pi}`;
+
+console.log(tmsg);
+console.log(mesg);
+
