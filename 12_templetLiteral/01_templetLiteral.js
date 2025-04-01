19b6d49ef45cfbf6070542109d9942280ab408ea
saurabh-1907 <147328092+saurabh-1907@users.noreply.github.com>
2025-04-01 13:42:26 +0000

    backtick

--- 12_templetLiteral/01_templetLiteral.js
+++ 12_templetLiteral/01_templetLiteral.js
@@ -9,8 +9,10 @@ var r = 10;
 const pi = 3.145;
 let mesg = "My name is";
 
-let tmsg = `My name is ${person.name}. I study at ${person.company}. The area of circle is ${r*r*pi}`;
-
+let tmsg = `My name is ${person.name}. 
+I study at ${person.company}. 
+The area of circle is ${r*r*pi}`;
+// while using backtick the javascript remembers spacing and new line
 console.log(tmsg);
 console.log(mesg);
 
